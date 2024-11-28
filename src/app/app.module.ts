f8dee89c7b51666db973c8025f8c41fd6c36eef5
ignacioTrezza <ignacio.trezza@gmail.com>
2024-11-28 15:26:17 -0300

    agrego componentes side y top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CredencialComponent } from './components/credencial/credencial.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { OthercomponentComponent } from './components/othercomponent/othercomponent.component';
 
 @NgModule({
-  declarations: [AppComponent, CredencialComponent],
+  declarations: [AppComponent, CredencialComponent, TopbarComponent, SidebarComponent, OthercomponentComponent],
   imports: [
     BrowserModule,
   ],
---
+++ src/app/components/othercomponent/othercomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-othercomponent',
+  templateUrl: './othercomponent.component.html',
+  styleUrls: ['./othercomponent.component.css']
+})
+export class OthercomponentComponent {
+
+}

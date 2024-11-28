3c28240eb42f2418085bff319568cadee212eb27
ignacioTrezza <ignacio.trezza@gmail.com>
2024-11-28 14:31:44 -0300

    agrego toogleFullScreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+interface FullScreenElement extends HTMLElement {
+  requestFullscreen(): Promise<void>;
+  exitFullscreen(): Promise<void>;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'credencial';
   nombre: string = 'Juan';
@@ -12,4 +19,15 @@ export class AppComponent {
   dni: string = '25.489.268';
   socio: number = 5468;
   vencimiento: string = '14/05/1975';
+
+  toggleFullScreen() {
+    const elem: FullScreenElement | null = document.querySelector('.credencial-container');
+    if (!document.fullscreenElement) {
+      elem?.requestFullscreen().catch((err: any) => {
+        alert(`Error al intentar activar el modo pantalla completa: ${err.message} (${err.name})`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
 }

a244254eb5b3c77da7900b26dad797274a808593
ignacioTrezza <ignacio.trezza@gmail.com>
2024-11-28 14:20:29 -0300

    agrego socio y ajusto tamaños. Sin landscape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,6 @@ export class AppComponent {
   nombre: string = 'Juan';
   apellido: string = 'Gomez';
   dni: string = '25.489.268';
+  socio: number = 5468;
   vencimiento: string = '14/05/1975';
 }

c04d6ec1d46286249aeddac91ac832915c7c8edc
ignacioTrezza <ignacio.trezza@gmail.com>
2024-11-29 01:17:26 -0300

    modo pantalla completa y restaura

--- src/app/components/credencial/credencial.component.ts
+++ src/app/components/credencial/credencial.component.ts
@@ -22,31 +22,36 @@ export class CredencialComponent {
 @Input() vencimiento: string = '';
 
   private buttonTimeout: any;
+  private isFullscreen: boolean = false;
 
   toggleFullScreen() {
     const elem: FullScreenElement | null = document.querySelector('.credencial-container');
     const credencialElem: HTMLElement | null = document.querySelector('.credencial');
+    
     if (!document.fullscreenElement) {
       elem?.requestFullscreen().catch((err: any) => {
         alert(`Error al intentar activar el modo pantalla completa: ${err.message} (${err.name})`);
       });
       credencialElem?.classList.add('credencial-fullscreen');
+      this.isFullscreen = true;
     } else {
       document.exitFullscreen();
       credencialElem?.classList.remove('credencial-fullscreen');
+      this.isFullscreen = false;
     }
+    this.showButton(); // Mostrar el botón al cambiar el modo
   }
 
   showButton() {
     const button = document.querySelector('.full-screen-button');
     button?.classList.add('show');
     
-    // Limpiamos el timeout anterior si existe
+    // Limpiar el timeout existente si hay uno
     if (this.buttonTimeout) {
       clearTimeout(this.buttonTimeout);
     }
     
-    // Establecemos un nuevo timeout
+    // Configurar nuevo timeout para ocultar el botón después de 2 segundos
     this.buttonTimeout = setTimeout(() => {
       button?.classList.remove('show');
     }, 2000);

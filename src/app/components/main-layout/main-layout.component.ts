4b614a8ad8a917467628ad60eda84ae0c5ff9378
ignacioTrezza <ignacio.trezza@gmail.com>
2024-11-29 02:06:04 -0300

    Todo en posicion

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class MainLayoutComponent {
   title = 'credencial';
-  nombre: string = 'Juan';
-  apellido: string = 'Gomez';
+  nombre: string = 'Juan Diego';
+  apellido: string = 'Gomez de Sol';
   dni: string = '25.489.268';
   socio: number = 123456;
   vencimiento: string = '14/05/1975';
